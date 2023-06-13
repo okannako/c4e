688e237f90f18088850c8f985583c994b89d1e7d
NakoTurk <filmoloji89@gmail.com>
2023-06-14 01:57:31 +0300

    Create c4emainnet.sh

---
+++ c4emainnet.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+echo -e "\033[0;37m"
+echo "============================================================================================================"
+echo " #####   ####        ####        ####  ####    ######    ##########  ####    ####  ###########   ####  ####"
+echo " ######  ####       ######       #### ####    ########   ##########  ####    ####  ####   ####   #### ####"
+echo " ####### ####      ###  ###      ########    ####  ####     ####     ####    ####  ####   ####   ########"   
+echo " #### #######     ##########     ########   ####    ####    ####     ####    ####  ###########   ########"
+echo " ####  ######    ############    #### ####   ####  ####     ####     ####    ####  ####  ####    #### ####"  
+echo " ####   #####   ####      ####   ####  ####   ########      ####     ############  ####   ####   ####  ####"
+echo " ####    ####  ####        ####  ####   ####    ####        ####     ############  ####    ####  ####   ####"
+echo "============================================================================================================"
+echo -e '\e[36mTwitter :\e[39m' https://twitter.com/NakoTurk
+echo -e '\e[36mGithub  :\e[39m' https://github.com/okannako
+echo -e '\e[36mYoutube :\e[39m' https://www.youtube.com/@CryptoChainNakoTurk
+echo -e "\e[0m"
+sleep 5
+
+echo -e "\e[1m\e[32m>>>Güncellemeler<<< \e[0m" && sleep 2
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential git ncdu -y
+sudo apt install make -y && cd $HOME
+sleep 1
+
+echo -e "\e[1m\e[32m>>>Go Yükleniyor<<< \e[0m" && sleep 2
+
+ver="1.20.3"
+cd $HOME
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+rm "go$ver.linux-amd64.tar.gz"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+echo -e "\e[1m\e[32m>>>İlgili Dosyalar Yükleniyor<<< \e[0m" && sleep 2
+git clone --depth 1 --branch  v1.2.0  https://github.com/chain4energy/c4e-chain.git
+cd c4e-chain/
+make install
+c4ed version
+sleep 1
+
+export CHAINID=perun-1
+
+echo "MONIKER:"
+read MONIKER
+echo export MONIKER=${MONIKER} >> $HOME/.bash_profile
+
+c4ed init $MONIKER --chain-id $CHAINID
+rm -rf ~/.c4e-chain/config/genesis.json
+
+git clone https://github.com/chain4energy/c4e-chains.git
+cd c4e-chains/$CHAINID
+cp genesis.json ~/.c4e-chain/config/
+
+peers="5a0e2f1a0541ba34cab61f64b62faaeb4113f2df@65.108.6.45:61056"
+sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$peers\"/" $HOME/.c4e-chain/config/config.toml
+sed -i 's/max_num_inbound_peers =.*/max_num_inbound_peers = 50/g' $HOME/.c4e-chain/config/config.toml
+sed -i 's/max_num_outbound_peers =.*/max_num_outbound_peers = 50/g' $HOME/.c4e-chain/config/config.toml
+
+pruning="custom" && \
+pruning_keep_recent="100" && \
+pruning_keep_every="0" && \
+pruning_interval="50" && \
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" ~/.c4e-chain/config/app.toml && \
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" ~/.c4e-chain/config/app.toml && \
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" ~/.c4e-chain/config/app.toml && \
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" ~/.c4e-chain/config/app.toml
+
+sudo tee /etc/systemd/system/c4ed.service > /dev/null <<EOF
+[Unit]
+Description=c4e
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which c4ed) start
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cd $HOME
+apt install lz4
+sudo systemctl stop c4ed
+cp $HOME/.c4e-chain/data/priv_validator_state.json $HOME/.c4e-chain/priv_validator_state.json.backup
+rm -rf $HOME/.c4e-chain/data
+curl -o - -L http://c4e.snapshot.stavr.tech:1018/c4e/c4e-snap.tar.lz4 | lz4 -c -d - | tar -x -C $HOME/.c4e-chain --strip-components 2
+mv $HOME/.c4e-chain/priv_validator_state.json.backup $HOME/.c4e-chain/data/priv_validator_state.json
+wget https://raw.githubusercontent.com/obajay/nodes-Guides/main/C4E/genesis.json -O $HOME/.c4e-chain/config/genesis.json
+
+sudo systemctl daemon-reload
+sudo systemctl enable c4ed
+sudo systemctl restart c4ed && sudo journalctl -u c4ed -f -o cat
